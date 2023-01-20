583f62dde628789626e1b431e43e0c5f774d84cd
JoannaMoussa <joannamoussa.jm@gmail.com>
2023-01-20 17:31:12 +0100

    Add listing page and watchlist feature

--- auctions/urls.py
+++ auctions/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("createListing", views.create_listing, name="create_listing")
+    path("createListing", views.create_listing, name="create_listing"),
+    path("listing/<int:listing_id>", views.listing_page, name="listing_page"),
+    path("addToWatchlist", views.add_to_watchlist, name="add_to_watchlist"),
+    path("removeFromWatchlist", views.remove_from_watchlist, name="remove_from_watchlist"),
+    path("addBid", views.add_bid, name="add_bid")
 ]
--- auctions/views.py
+++ auctions/views.py
@@ -8,13 +8,18 @@ from django import forms
 
 MAX_DESCRIPTION_LEN = 100
 
-# Creating a django form that allows the user to create a new listing.
+# django form that allows the user to create a new listing.
 class NewListingForm(forms.Form):
     title = forms.CharField(label="Listing Title", required=True, widget=forms.TextInput(attrs={'class': 'form-control col-2 mb-3'}))
     description = forms.CharField(label="Description", required=True, widget=forms.Textarea(attrs={'class': 'form-control col-4 mb-3', 'rows': '5'}))
     initial_bid = forms.FloatField(label="Starting bid", required=True, widget=forms.NumberInput(attrs={'class': 'form-control col-2 mb-3'}))
     image_url = forms.URLField(label="Image URL", widget=forms.URLInput(attrs={'class': 'form-control col-2 mb-3'}))
     category = forms.ChoiceField(label="Category", choices=CATEGORIES, widget=forms.Select(attrs={'class': 'form-control col-2 mb-3'}))
+
+
+# django form that allows the user to add a bid for a listing.
+class PlaceBidForm(forms.Form):
+    bid = forms.FloatField(label="Bid", required=True, widget=forms.NumberInput(attrs={'class': 'form-control col-2 mb-3', 'placeholder': 'Bid'}))
 
 
 def index(request):
@@ -97,3 +102,52 @@ def create_listing(request):
         return render(request, "auctions/create_listing.html", {
             "form": NewListingForm()
         })
+
+
+def listing_page(request, listing_id):
+    current_listing = Listing.objects.get(id=listing_id)
+    if request.user.is_authenticated:
+        current_user = request.user
+        if current_listing in current_user.watchlist.all():
+            watchlist_button = False
+        else:
+            watchlist_button = True
+        return render(request, "auctions/listing_page.html", {
+            "current_listing": current_listing,
+            "form": PlaceBidForm(),
+            "watchlist_button": watchlist_button
+        })
+    else:
+        return render(request, "auctions/listing_page.html", {
+            "current_listing": current_listing,
+            "form": PlaceBidForm()
+        })
+
+
+# TODO @login_required
+def add_to_watchlist(request):
+    if request.method == "POST":
+        current_user = request.user
+        listing_id = request.POST["listing_id"]
+        current_listing = Listing.objects.get(id=listing_id)
+        current_user.watchlist.add(current_listing)
+        # TODO show a success message to the user that the listing item was added to the watchlist.
+        return HttpResponseRedirect(reverse("auctions:listing_page", kwargs={'listing_id': listing_id}))
+    else: # GET
+        return HttpResponseRedirect(reverse("auctions:index"))
+
+
+def remove_from_watchlist(request):
+    if request.method == "POST":
+        current_user = request.user
+        listing_id = request.POST["listing_id"]
+        current_listing = Listing.objects.get(id=listing_id)
+        current_user.watchlist.remove(current_listing)
+        # TODO show a success message to the user that the listing item was added to the watchlist.
+        return HttpResponseRedirect(reverse("auctions:listing_page", kwargs={'listing_id': listing_id}))
+    else: # GET
+        return HttpResponseRedirect(reverse("auctions:index"))
+
+
+def add_bid(request):
+    pass
